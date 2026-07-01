19a54a2653830c3cedaebcb8b90112d324136376
RJRP44 <romainpouvreau@orange.fr>
2026-07-01 22:14:13 +0200

    Update database to add data annotation for neural network training

--- db.sql
+++ db.sql
@@ -1,5 +1,7 @@
 -- Create tables
 
+CREATE TYPE movement AS ENUM('Inside', 'Outside', 'Other');
+
 CREATE TABLE cats
 (
     cat VARCHAR(5) PRIMARY KEY
@@ -9,36 +11,48 @@ CREATE TABLE sessions
 (
     cat       VARCHAR(5) REFERENCES cats,
     timestamp TIMESTAMP,
-    video BOOLEAN,
+    video     BOOLEAN,
+    annotated BOOLEAN,
     PRIMARY KEY (cat, timestamp)
 );
 
+CREATE TABLE annotations
+(
+    cat        VARCHAR(5) REFERENCES cats,
+    timestamp  TIMESTAMP,
+    mcu_ms     INTEGER CHECK ( mcu_ms > 0 ),
+    cluster_id INTEGER CHECK ( cluster_id >= 0 ),
+    result     movement NOT NULL,
+    PRIMARY KEY (cat, timestamp, mcu_ms, cluster_id),
+    FOREIGN KEY (cat, timestamp) REFERENCES sessions (cat, timestamp)
+)
+
 CREATE TABLE raw_data
 (
-    cat VARCHAR(5),
+    cat       VARCHAR(5),
     timestamp TIMESTAMP,
-    mcu_ms INTEGER CHECK ( mcu_ms > 0 ),
-    data TEXT,
+    mcu_ms    INTEGER CHECK ( mcu_ms > 0 ),
+    data      TEXT,
     PRIMARY KEY (cat, timestamp, mcu_ms),
-    FOREIGN KEY (cat, timestamp) REFERENCES sessions(cat, timestamp)
+    FOREIGN KEY (cat, timestamp) REFERENCES sessions (cat, timestamp)
 );
 
 CREATE TABLE calibration_data
 (
-    cat VARCHAR(5),
+    cat       VARCHAR(5),
     timestamp TIMESTAMP,
-    data TEXT,
+    data      TEXT,
     PRIMARY KEY (cat, timestamp),
-    FOREIGN KEY (cat, timestamp) REFERENCES sessions(cat, timestamp)
+    FOREIGN KEY (cat, timestamp) REFERENCES sessions (cat, timestamp)
 );
 
 CREATE TABLE logs
 (
-    id SERIAL PRIMARY KEY,
-    cat VARCHAR(5),
+    id        SERIAL PRIMARY KEY,
+    cat       VARCHAR(5),
     timestamp TIMESTAMP,
-    mcu_ms INTEGER CHECK ( mcu_ms > 0 ),
-    level INTEGER,
-    log TEXT,
-    FOREIGN KEY (cat, timestamp) REFERENCES sessions(cat, timestamp)
+    mcu_ms    INTEGER CHECK ( mcu_ms > 0 ),
+    level     INTEGER,
+    log       TEXT,
+    FOREIGN KEY (cat, timestamp) REFERENCES sessions (cat, timestamp)
 );
